e94d6b18c3f592f63e5ef2b08f05c0bc05066d3a
Albert Ho <albert.ho@gempages.help>
2024-08-13 20:55:10 +0700

    Config onedarkpro

--- lua/plugins/colorscheme/onedarkpro.lua
+++ lua/plugins/colorscheme/onedarkpro.lua
@@ -2,14 +2,23 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000, -- Ensure it loads first
-    highlights = {
-      Constant = { fg = "${yellow}" },
-    },
-    {
-      "LazyVim/LazyVim",
-      opts = {
-        colorscheme = "onedark",
-      },
+    config = function()
+      require("onedarkpro").setup({
+        highlights = {
+          ["@tag.tsx"] = { fg = "${yellow}" },
+          ["@lsp.typemod.variable.readonly.typescriptreact"] = { fg = "${yellow}" },
+          ["@tag.builtin.tsx"] = { link = "Identifier" },
+        },
+        options = {
+          cursorline = true,
+        },
+      })
+    end,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "onedark",
     },
   },
 }
